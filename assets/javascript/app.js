dbb6f33e4934021cbef5a0988f43e76f9f1b4d0c
amberorenstein <amber.orenstein@gmail.com>
2017-06-24 08:10:39 -0600

    accurate time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 	var database = firebase.database()
 
-
+	$(".panel").css("padding", "0px");
 	$("#addButton").on('click', function(event){
 		event.preventDefault();
 
@@ -22,12 +22,11 @@ $(document).ready(function() {
 		objectToUpdate.destination = $('#destination-input').val().trim();
 		objectToUpdate.firstTrain = $('#first-train').val().trim();
 		objectToUpdate.frequency = $('#frequency-input').val().trim();
-		objectToUpdate.firstTrainConverted = moment(objectToUpdate.firstTrain, "hh:mm").subtract(1, "years");
-		objectToUpdate.currentTime = moment();
-		objectToUpdate.diffTime = moment().diff(moment(objectToUpdate.firstTrainConverted), "minutes");
+		objectToUpdate.firstTrainConverted = moment(objectToUpdate.firstTrain, "HH:mm").subtract(10, "years").format("X");
+		objectToUpdate.diffTime = moment().diff(moment.unix(objectToUpdate.firstTrainConverted), "minutes");
 		objectToUpdate.timeLeft = (objectToUpdate.diffTime) % (objectToUpdate.frequency);
 		objectToUpdate.minUntilTrain = (objectToUpdate.frequency) - (objectToUpdate.timeLeft);
-		objectToUpdate.nextTrainMinutes = moment(objectToUpdate.minUntilTrain).format("hh:mm");
+		objectToUpdate.nextTrainMinutes = moment().add(objectToUpdate.minUntilTrain, "m").format("HH:mm");
 
 		console.log(objectToUpdate);
 
@@ -35,7 +34,9 @@ $(document).ready(function() {
 			name: objectToUpdate.name,
 			destination: objectToUpdate.destination,
 			firstTrain: objectToUpdate.firstTrain,
-			frequency: objectToUpdate.frequency
+			frequency: objectToUpdate.frequency,
+			nextArrival: objectToUpdate.nextTrainMinutes,
+			minAway: objectToUpdate.minUntilTrain
 		});
 	});
 
@@ -51,16 +52,20 @@ $(document).ready(function() {
 		newDiv1.append(a.destination);
 		var newDiv2=$('<div class="col-md-2">');
 		newDiv2.append(a.frequency);
+		var newDiv3=$('<div class="col-md-2">');
+		newDiv3.append(a.nextArrival);
+		var newDiv4=$('<div class="col-md-2">');
+		newDiv4.append(a.minAway);		
 		
-		
+
 		var superDiv=$('<div class="row">');
 		superDiv.append(newDiv);
 		superDiv.append(newDiv1);
 		superDiv.append(newDiv2);
-
+		superDiv.append(newDiv3);
+		superDiv.append(newDiv4);
 
 		$(".chartData").prepend(superDiv);
-
 
 	}, 
 	function(errorObject) {          
